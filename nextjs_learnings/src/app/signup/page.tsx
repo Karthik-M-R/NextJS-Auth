e0b1017b97dfaedc136f286390a8891c0b28c641
Karthik <karthikmr135@gmail.com>
2026-03-21 23:03:53 +0530

    NextJS setup done

---
+++ nextjs_learnings/src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from 'next/link'
+import React from "react"
+import { useRouter } from 'next/navigation';
+import {userForm} from "react-hook-form"
+
+export default function SignupPage() {
+  return (
+    <div>
+      <h1>Signup</h1>
+      
+    </div>
+  );
+}
