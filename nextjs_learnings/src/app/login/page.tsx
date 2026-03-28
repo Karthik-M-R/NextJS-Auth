69f3286a0f2f58f035f73271a269432fae64f380
Karthik <karthikmr135@gmail.com>
2026-03-28 14:35:26 +0530

    signup and login done directing to profile

--- nextjs_learnings/src/app/login/page.tsx
+++ nextjs_learnings/src/app/login/page.tsx
@@ -1,24 +1,60 @@
 "use client";
 import Link from 'next/link'
-import React from "react"
+import React,{useEffect} from "react"
 import { useRouter } from 'next/navigation';
-import { axios } from 'axios';
+import axios  from 'axios';
+import toast from 'react-hot-toast';
 
 export default function LoginPage() {
+  const router =useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     
   })
+  const[buttonDisabled, setButtonDisabled] =
+   React.useState(false);
+   const[loading, setLoading] = React.useState(false);
 
   const onLogin= async () => {
-    // Logic goes here
+    try{
+      setLoading(true);
+      const response = await axios.post('/api/users/login', user);
+      console.log("Login response:", response);
+      toast.success("login successful")
+      setTimeout(() => {
+        router.push('/profile');
+      }, 500);
+
+
+    }
+    catch(error:any){
+      console.log("Login failed",error);
+      console.log("Error response:", error.response);
+      toast.error(error.response?.data?.error || error.message);
+
+
+    }
+    finally{
+      setLoading(false);
+    }
+   
   }
+
+  useEffect(()=>{
+    if(user.email.length>0 && user.password.length>0){
+      setButtonDisabled(false);
+    }else{
+      setButtonDisabled(true);
+    }
+  },[user])
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50 text-black">
       <div className="p-8 bg-white shadow-md rounded-lg flex flex-col w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center mb-4">Login</h1>
+        <h1 className="text-3xl font-bold text-center mb-4">
+          {loading ? "Logging in..." : "Login"  }
+        </h1>
         <hr className="mb-6" />
 
 
--- nextjs_learnings/src/app/signup/page.tsx
+++ nextjs_learnings/src/app/signup/page.tsx
@@ -1,24 +1,51 @@
 "use client";
 import Link from 'next/link'
-import React from "react"
+import React,{useEffect} from "react"
 import { useRouter } from 'next/navigation';
-import { axios } from 'axios';
+import axios  from 'axios';
+import { toast } from 'react-hot-toast/headless';
 
 export default function SignupPage() {
+  const router=useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   })
+  const [buttonDisabled,setButtonDisabled] = React.useState(false);
+  const[loading,setLoading] = React.useState(false);
 
   const onSignup = async () => {
-    // Logic goes here
+    try{
+         setLoading(true);
+         const response=await axios.post("/api/users/signup", user);
+         console.log("Signup success",response.data);
+         toast.success("Signup successful! Redirecting to login...");
+         router.push("/login");
+    }
+    catch(error:any){
+      console.log("Signup failed", error);
+      console.log("Error response:", error.response);
+      toast.error(error.response?.data?.error || error.message);
+
+    }
+    finally{
+      setLoading(false);
+    }
   }
 
+  useEffect(()=>{
+    if(user.email.length>0 && user.password.length>0 && user.username.length>0){
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+
+  },[user])
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50 text-black">
       <div className="p-8 bg-white shadow-md rounded-lg flex flex-col w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center mb-4">Signup</h1>
+        <h1 className="text-3xl font-bold text-center mb-4">{loading?"Signing up...":"Signup"}</h1>
         <hr className="mb-6" />
 
         <label className="text-sm font-semibold mb-1" htmlFor="username">Username</label>
@@ -55,7 +82,7 @@ export default function SignupPage() {
           onClick={onSignup}
           className="p-2 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-700 transition duration-200 active:scale-95"
         >
-          Signup
+          {buttonDisabled ? "NO Signup" : "Signup"}
         </button>
 
         <p className="mt-4 text-center text-sm">
